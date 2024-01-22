4c66bcaadfacbed870613a61c380007ffd26ef16
Caleb Rutan <crutan@gmail.com>
2024-01-22 19:25:03 +0000

    add exercise tabs

--- src/app/exercises/page.tsx
+++ src/app/exercises/page.tsx
@@ -1,7 +1,27 @@
+import { Prisma, BodyPart, PrismaClient } from "@prisma/client"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+const prisma = new PrismaClient()
+
 export default async function Page() {
+  const parts = [ BodyPart.Arms, BodyPart.Back, BodyPart.Chest, BodyPart.Core, BodyPart.Legs, BodyPart.Shoulders ]
+
   return (
     <main>
-      <p>Exercises</p>
+      <h2>Exercises</h2>
+      <Tabs defaultValue={parts[0]} className="w-full">
+        <TabsList>
+          {parts.map((part) => (
+            <TabsTrigger key={part} value={part}>{part}</TabsTrigger>
+          ))}
+        </TabsList>
+        {parts.map((part) => (
+          <TabsContent key={part} value={part}>
+            Tab content for {part}
+          </TabsContent>
+        ))}
+      </Tabs>
+      
     </main>
   )
 }
